0e7707b42f86fd55deebf8db25ce396a2d352487
kmantzoukas <kmantzoukas.uk@gmail.com>
2020-10-03 18:44:28 +0300

    Delete folder with the results

--- code/AnonymizeData/src/main/scala/uk/ac/city/services/AnonymizeData.scala
+++ code/AnonymizeData/src/main/scala/uk/ac/city/services/AnonymizeData.scala
@@ -3,19 +3,17 @@ package uk.ac.city.services
 import java.math.BigInteger
 import java.security.MessageDigest
 import org.apache.spark.sql.SparkSession
-import sys.process._
+import scala.sys.process._
 
 object AnonymizeData {
 
-
-
   def main(args: Array[String]) {
 
-    "gsutil -m rm -r gs://dataproc-staging-us-central1-658776204196-yztfnikr/output/*" !
+    "hdfs dfs -rm -r " + "/user/root/output/1"!
 
     val spark = SparkSession.builder.getOrCreate
 
-    val input = spark.sparkContext.textFile(args(0), 20)
+    val input = spark.sparkContext.textFile(args(0), args(2).toInt)
     input.map(m => {
       /*
       Read every line of the input and split it
@@ -41,10 +39,10 @@ object AnonymizeData {
         new BigInteger(1, md.digest(temp(2).getBytes("UTF-8"))))
 
       /*
-      Return the same line with except for the name and surname that has been SHA-256 hashed
+      Return the same line as the original except for the name and surname that has been SHA-256 hashed
        */
       temp.mkString(" ")
-    })saveAsTextFile(args(1))
+    }).saveAsTextFile(args(1))
 
     spark.stop
   }
